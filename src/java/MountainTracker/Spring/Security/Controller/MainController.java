aa9f4bebe10f8cb585e6c0cb84f497f60e52cec3
David <david.gutierrez@almis.com>
2015-06-10 10:06:17 +0200

    Create MainController.java

---
+++ src/java/MountainTracker/Spring/Security/Controller/MainController.java
@@ -0,0 +1,90 @@
+package MountainTracker.Spring.Security.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.LockedException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+
+	@RequestMapping(value = { "/", "/MountainTracker" }, method = RequestMethod.GET)
+	public String defaultPage() {
+		return "index";
+	}
+
+	@RequestMapping(value = "/MountainTracker/gestion**", method = RequestMethod.GET)
+	public ModelAndView adminPage() {
+
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Spring Security + Hibernate Example");
+		model.addObject("message", "This page is for ROLE_ADMIN only!");
+		model.setViewName("admin");
+
+		return model;
+
+	}
+
+	@RequestMapping(value = "/MountainTracker/login", method = RequestMethod.GET)
+	public ModelAndView login(@RequestParam(value = "error", required = false) String error,
+			@RequestParam(value = "logout", required = false) String logout, HttpServletRequest request) {
+
+		ModelAndView model = new ModelAndView();
+		if (error != null) {
+			model.addObject("error", getErrorMessage(request, "SPRING_SECURITY_LAST_EXCEPTION"));
+            model.setViewName("login");
+        }
+
+		if (logout != null) {
+			model.addObject("msg", "You've been logged out successfully.");
+            model.setViewName("index");
+		}
+
+		return model;
+
+	}
+
+	// customize the error message
+	private String getErrorMessage(HttpServletRequest request, String key) {
+
+		Exception exception = (Exception) request.getSession().getAttribute(key);
+
+		String error = "";
+		if (exception instanceof BadCredentialsException) {
+			error = "Invalid username and password!";
+		} else if (exception instanceof LockedException) {
+			error = exception.getMessage();
+		} else {
+			error = "Invalid username and password!";
+		}
+
+		return error;
+	}
+
+	// for 403 access denied page
+	@RequestMapping(value = "/AgenciaWeb/403", method = RequestMethod.GET)
+	public ModelAndView accesssDenied() {
+
+		ModelAndView model = new ModelAndView();
+
+		// check if user is login
+		/*Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (!(auth instanceof AnonymousAuthenticationToken)) {
+			UserDetails userDetail = (UserDetails) auth.getPrincipal();
+			System.out.println(userDetail);
+
+			model.addObject("username", userDetail.getUsername());
+
+		}*/
+
+		model.setViewName("index");
+		return model;
+
+	}
+
+}
